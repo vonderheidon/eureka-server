9f98185f199fad5460114c85a1b5bd31bed2aff1
vonderheidon <jefferson.sg91@hotmail.com>
2025-09-12 21:36:28 -0300

    first commit

---
+++ src/test/java/br/edu/catolicapb/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.catolicapb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
